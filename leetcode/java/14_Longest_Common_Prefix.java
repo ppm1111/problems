5779f555859e05f7471d1d18b21647661f8ab940
ppm1111 <ppqqbbdd1111@gmail.com>
2020-06-02 01:34:12 +0800

    14_Longest_Common_Prefix

--- leetcode/java/14_Longest_Common_Prefix.java
+++ leetcode/java/14_Longest_Common_Prefix.java
@@ -29,3 +29,20 @@ class Solution {
          return strs[0].substring(0, min);
      }
  }
+
+ // other solution
+ class Solution {
+
+    public String longestCommonPrefix(String[] strs) {
+        if ( strs.length == 0 ) return "";
+        final StringBuilder sb = new StringBuilder(strs[0]);
+        for (int i = 1; i < strs.length; ++i ){
+            final char[] arr = strs[i].toCharArray();
+            for ( int j = 0; j < arr.length && j < sb.length() ; ++j ){
+                if ( sb.charAt(j) != arr[j] ) sb.setLength(j);
+            }
+            if ( sb.length() > arr.length ) sb.setLength(arr.length);
+        }
+        return sb.toString();
+    }
+}
