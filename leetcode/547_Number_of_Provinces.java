789d67ed4d4f3f260c9408c7a0ae1134177665c6
ppm1111 <ppqqbbdd1111@gmail.com>
2021-01-30 15:54:40 +0800

    547. Number of Provinces

---
+++ leetcode/547_Number_of_Provinces.java
@@ -0,0 +1,26 @@
+// bfs + queue
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int total = isConnected.length;
+        int[] all = new int[total];
+        Queue<Integer> q = new LinkedList<Integer>();
+        
+        int c = 0;
+        for (int b=0;b<total;b++) {
+            if (all[b] == 0) {
+                c++;
+                q.offer(b);
+                while(!q.isEmpty()) {
+                    int pop = q.poll();
+                    for (int a=0;a<total;a++) {
+                        if (isConnected[pop][a] == 1 && all[a]==0) {
+                            q.offer(a);
+                            all[a] = 1;
+                        }
+                    }
+                }
+            }
+        }
+        return c;
+    }
+}
